2336ab484388c8ddc9edb44ab23981602ebcdfe7
Brakau-TD <torsten.drever@outlook.de>
2023-07-08 13:44:29 +0200

    still working on pixel perfect lines

--- pixeldraw/pixeldraw.py
+++ pixeldraw/pixeldraw.py
@@ -27,95 +27,111 @@ class DrawCanvas():
         self.size = size
         self.screen = pygame.display.set_mode((size[0], size[1]))
         self.items = []
+        #self.bg = pygame.image.load("bgscreen.png").convert_alpha()
         self.bg = pygame.image.load("grayscreen.png").convert()
         self.bg = pygame.transform.scale(self.bg, (size[0], size[1]))
         self.tools = tools
         self.selected = selected
         self.deselected = deselected
-        self.pdt = PixelDrawTools()
+        self.linelist = []
+        self.colors = []
+        self.color = (0, 0, 0)
+        self.state = "pixel"
+        self.pixelsize = 20
+        self.pdt = PixelDrawTools(self.pixelsize, self.size, self.screen)
         self.userevents = UserEvents()
-        self.make_lowresolution()
+        # self.make_lowresolution()
 
-    def make_lowresolution(self):
-        lr_array = []
-        temp = []
-        line = []
-        for i in range(self.size[1] // 10):
-            for y in range(self.size[0] // 10):
-                temp.append("")
-            line.append(temp)
-            temp = []
+    # def make_lowresolution(self):
+    #     lr_array = []
+    #     temp = []
+    #     line = []
+    #     for i in range(self.size[1] // 10):
+    #         for y in range(self.size[0] // 10):
+    #             temp.append("")
+    #         line.append(temp)
+    #         temp = []
 
     def drawcolorpalette(self):
         for i, element in enumerate(palettecolors):
-            pygame.draw.rect(self.screen, element, pygame.Rect((i * 20, 0), (20, 20)))
+            pygame.draw.rect(self.screen, element,
+                             pygame.Rect((i * self.pixelsize, 0), (self.pixelsize, self.pixelsize)))
 
     def drawtools(self):
         for i, element in enumerate(self.tools):
-            self.screen.blit(element, (i * 20, 20))
+            self.screen.blit(element, (i * self.pixelsize, self.pixelsize))
 
     def drawcanvas(self, items: list):
         self.screen.blit(self.bg, (0, 0))
-        colors = []
-        color = (0,0,0)
-        linestart = []
-        lineend = []
-        linelist = []
-        state = "pixel"
-        indices = []
+        linestart, lineend, indices = [], [], []
         running = True
         while running == True:
             self.screen.blit(self.bg, (0, 0))
             mousepos = pygame.mouse.get_pos()
-            pointer_rect = pygame.Rect(((mousepos[0] // 20 * 20, mousepos[1] // 20 * 20)), (20, 20))
+            pointer_rect = pygame.Rect(
+                ((mousepos[0] // self.pixelsize * self.pixelsize, mousepos[1] // self.pixelsize * self.pixelsize)),
+                (self.pixelsize, self.pixelsize))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                     break
                 if event.type == pygame.KEYDOWN:
-                    color = self.userevents.keydown_colors(event, color)
+                    self.color = self.userevents.keydown_colors(event, self.color)
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1 and mousepos[1] < 20:
-                        color = palettecolors[mousepos[0] // 20]
-                    elif event.button == 1 and 20 <= mousepos[1] <= 40:
+                    if event.button == 1 and mousepos[1] < self.pixelsize:
+                        self.color = palettecolors[mousepos[0] // self.pixelsize]
+                    elif event.button == 1 and self.pixelsize <= mousepos[1] <= self.pixelsize * 2:
                         print("options")
-                        if 0 < mousepos[0] < 20 and state != "erase":
-                            state = "erase"
+                        if 0 < mousepos[0] < self.pixelsize and self.state != "erase":
+                            self.state = "erase"
                             self.tools[0] = self.selected[0]
 
-                        elif 0 < mousepos[0] < 20 and state == "erase":
-                            state = "pixel"
+                        elif 0 < mousepos[0] < self.pixelsize and self.state == "erase":
+                            self.state = "pixel"
                             self.tools[0] = self.deselected[0]
-                    elif event.button == 1 and state == "erase":
+                    elif event.button == 1 and self.state == "erase":
                         try:
-                            pixelindex = indices.index(((mousepos[0] // 20) * 20, (mousepos[1] // 20) * 20))
-                            colors.pop(pixelindex)
+                            pixelindex = indices.index(((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                                        (mousepos[1] // self.pixelsize) * self.pixelsize))
+                            self.colors.pop(pixelindex)
                             items.pop(pixelindex)
                             indices.pop(pixelindex)
                         except ValueError as e:
                             print(e)
-                    elif event.button == 1 and state == "pixel":
-                        items.append(pygame.Rect(((mousepos[0] // 20) * 20, (mousepos[1] // 20) * 20), (20, 20)))
-                        colors.append(color)
-                        indices.append(((mousepos[0] // 20) * 20, (mousepos[1] // 20) * 20))
-                        state = "pixel"
-                    elif event.button == 1 and state == "line":
-                        lineend = ((mousepos[0] // 20) * 20, (mousepos[1] // 20) * 20)
-                        linecolor = color
-                        linelist.append([linestart, lineend, color])
-                        state = "pixel"
-                    elif event.button == 3 and mousepos[1] > 40:
-                        state = "line"
-                        linestart = ((mousepos[0] // 20) * 20, (mousepos[1] // 20) * 20)
+                        try:
+                            for i in self.linelist:
+                                if i[0] == ((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                            (mousepos[1] // self.pixelsize) * self.pixelsize):
+                                    self.linelist.remove(i)
+                        except ValueError as e:
+                            print(e)
+                    elif event.button == 1 and self.state == "pixel":
+                        items.append(pygame.Rect(((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                                  (mousepos[1] // self.pixelsize) * self.pixelsize),
+                                                 (self.pixelsize, self.pixelsize)))
+                        self.colors.append(self.color)
+                        indices.append(((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                        (mousepos[1] // self.pixelsize) * self.pixelsize))
+                        self.state = "pixel"
+                    elif event.button == 1 and self.state == "line":
+                        lineend = ((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                   (mousepos[1] // self.pixelsize) * self.pixelsize)
+                        linecolor = self.color
+                        self.linelist.append([linestart, lineend, self.color])
+                        self.state = "pixel"
+                    elif event.button == 3 and mousepos[1] > self.pixelsize * 2:
+                        self.state = "line"
+                        linestart = ((mousepos[0] // self.pixelsize) * self.pixelsize,
+                                     (mousepos[1] // self.pixelsize) * self.pixelsize)
 
-            pygame.draw.rect(self.screen, color, pointer_rect)
-            # pygame.draw.rect(self.screen,WHITE,(mousepos[0],mousepos[1],50,50))
+            pygame.draw.rect(self.screen, self.color, pointer_rect)
+            self.pdt.draw_pixelated_lines(self.linelist)
+            if self.state == "line":
+                self.pdt.draw_temporary_line(linestart, mousepos)
             for i, element in enumerate(items):
-                pygame.draw.rect(self.screen, colors[i], element)
+                pygame.draw.rect(self.screen, self.colors[i], element)
 
-            for i, line in enumerate(linelist):
-                pygame.draw.line(self.screen, line[2], line[0], line[1], 20)
             self.drawcolorpalette()
             self.drawtools()
 
--- pixeldraw/pixeldrawtools.py
+++ pixeldraw/pixeldrawtools.py
@@ -1,24 +1,102 @@
-import math
+import pygame
+
 
 class PixelDrawTools:
-    def __init__(self):
+    def __init__(self, pixel_size: int, screen_size: tuple, screen: pygame.Surface):
+        self.pixelsize = pixel_size
+        self.screensize = screen_size
+        self.screen = screen
+
         pass
 
-    def angle_between_points(self, p1, p2):
-        d1 = p2[0] - p1[0]
-        d2 = p2[1] - p1[1]
-        if d1 == 0:
-            if d2 == 0:  # same points?
-                deg = 0
-            else:
-                deg = 0 if p1[1] > p2[1] else 180
-        elif d2 == 0:
-            deg = 90 if p1[0] < p2[0] else 270
-        else:
-            deg = math.atan(d2 / d1) / pi * 180
-            lowering = p1[1] < p2[1]
-            if (lowering and deg < 0) or (not lowering and deg > 0):
-                deg += 270
-            else:
-                deg += 90
-        return deg
+    def draw_pixelated_lines(self, lineslist: list):
+        lp = None
+        for line in lineslist:
+            (x1, y1), (x2, y2), color = line
+            dx = abs(x2 - x1)
+            dy = abs(y2 - y1)
+            sx = -1 if x1 > x2 else 1
+            sy = -1 if y1 > y2 else 1
+            err = dx - dy
+            line_pixels = []  # Store the pixels of the line
+
+            while x1 != x2 or y1 != y2:
+                line_pixels.append(((x1//self.pixelsize)*self.pixelsize, (y1//self.pixelsize)*self.pixelsize))
+                e2 = 2 * err
+                if e2 > -dy:
+                    err -= dy
+                    x1 += sx
+                if e2 < dx:
+                    err += dx
+                    y1 += sy
+            line_pixels.append((x2, y2))
+            line_pixels = list(set(line_pixels))
+            lp = self.remove_stacked_pixels(line_pixels.copy())
+            for pixel in lp:
+                pygame.draw.rect(self.screen, color, ((pixel[0]//self.pixelsize)*self.pixelsize,
+                                                      (pixel[1]//self.pixelsize)*self.pixelsize,
+                                                      self.pixelsize,
+                                                      self.pixelsize))
+        print(list(set(lp)))
+
+
+    def remove_stacked_pixels(self, line_pixels: list):
+        for i,pixel in enumerate(line_pixels):
+            if i == 0:
+                continue
+            if pixel[0] == line_pixels[i-1][0] or pixel[1] == line_pixels[i-1][1]:
+                line_pixels.pop(i-1)
+        return line_pixels
+
+
+        # for line in lineslist:
+        #     start_pos, end_pos = line[0], line[1]
+        #     color = line[2]
+        #     x1, y1 = start_pos
+        #     x2, y2 = end_pos
+        #
+        #     dx = abs(x2 - x1)
+        #     dy = abs(y2 - y1)
+        #     sx = 1 if x1 < x2 else -1
+        #     sy = 1 if y1 < y2 else -1
+        #
+        #     err = dx - dy
+        #
+        #     while x1 != x2 or y1 != y2:
+        #         pygame.draw.rect(self.screen, color, ((x1//self.pixelsize)*self.pixelsize,
+        #                                               (y1//self.pixelsize)*self.pixelsize,
+        #                                               self.pixelsize,
+        #                                               self.pixelsize))
+        #         e2 = 2 * err
+        #         if e2 > -dy:
+        #             err -= dy
+        #             x1 += sx
+        #         if e2 < dx:
+        #             err += dx
+        #             y1 += sy
+
+    def draw_temporary_line(self, linestart, lineend):
+        start_pos, end_pos = linestart, lineend
+        color = (211, 211, 211)
+        x1, y1 = start_pos
+        x2, y2 = end_pos
+
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+        sx = 1 if x1 < x2 else -1
+        sy = 1 if y1 < y2 else -1
+
+        err = dx - dy
+
+        while x1 != x2 or y1 != y2:
+            pygame.draw.rect(self.screen, color, ((x1 // self.pixelsize) * self.pixelsize,
+                                                  (y1 // self.pixelsize) * self.pixelsize,
+                                                  self.pixelsize,
+                                                  self.pixelsize))
+            e2 = 2 * err
+            if e2 > -dy:
+                err -= dy
+                x1 += sx
+            if e2 < dx:
+                err += dx
+                y1 += sy
